11f7e01eb5cb81ad151cce41b3db2f479d5ae978
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-10-24 13:20:35 -0300

    Fix email list order

--- src/app/_services/email.service.ts
+++ src/app/_services/email.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { DefaultResponse, EmailPayload } from '../_models/request-response.model';
 
@@ -13,7 +14,14 @@ export class EmailService {
   ) { }
 
   public list() {
-    return this.http.get<DefaultResponse<EmailPayload.Email[]>>(`${environment.baseUrl}/v1/email`);
+    return this.http
+      .get<DefaultResponse<EmailPayload.Email[]>>(`${environment.baseUrl}/v1/email`)
+      .pipe(
+        map((response) => {
+          response.result.sort((a, b) => ((a.sent === b.sent) ? 0 : a.sent ? 1 : -1));
+          return response;
+        })
+      );
   }
 
   public get(emailId: string) {
