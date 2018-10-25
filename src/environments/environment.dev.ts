b27936531a049ab696930a9d63be5433d6e79f75
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-10-25 07:11:34 -0300

    Fix to test

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,6 @@ import environmentModel from './environment.model';
 
 export const environment: environmentModel = {
   production: false,
-  baseUrl: 'http://localhost:8080/api'
+  // baseUrl: 'http://localhost:8080/api'
+  baseUrl: 'https://gauti-invoice-api.herokuapp.com/api'
 };
