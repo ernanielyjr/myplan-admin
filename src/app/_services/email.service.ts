9177cbf044d542be4c6e171ca950ca254dbba8b1
Ernani Ely Jr <ernani.gauti@gmail.com>
2020-11-09 13:29:11 -0300

    limit on email list

--- src/app/_services/email.service.ts
+++ src/app/_services/email.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { DefaultResponse, EmailPayload } from '../_models/request-response.model';
 
@@ -14,16 +13,7 @@ export class EmailService {
   ) { }
 
   public list() {
-    return this.http
-      .get<DefaultResponse<EmailPayload.Email[]>>(`${environment.baseUrl}/v1/email`)
-      .pipe(
-        map((response) => {
-          response.result.sort((a, b) => {
-            return (a.sent === b.sent) ? (a.createdAt > b.createdAt ? -1 : (a.createdAt < b.createdAt ? 1 : 0)) : (a.sent ? 1 : -1);
-          });
-          return response;
-        })
-      );
+    return this.http.get<DefaultResponse<EmailPayload.Email[]>>(`${environment.baseUrl}/v1/email?limit=50`);
   }
 
   public get(emailId: string) {
