6a9d1181ce214d7a56b158b2ab125c3c257d37af
Ernani Ely Jr. <12513504+ernanielyjr@users.noreply.github.com>
2026-04-03 10:27:35 -0300

    chore: update package.json for development and production settings
    - Added a new script for development (`dev`) to serve the application on port 4201.
    - Modified the `build` script to set the base href to `/admin/`.
    - Updated Node.js and npm engine versions to 14.21.3 and 6.14.18 respectively.
    - Changed `rxjs` version from `^6.0.0` to `6.0.0` for consistency.
    
    refactor: remove unused Router import in AuthorizeInterceptor
    
    - Removed the unused `Router` import from `authorize.interceptor.ts` to clean up the code.
    
    fix: update production environment base URL
    
    - Changed the `baseUrl` in `environment.prod.ts` to use a relative path (`/api`) instead of the full URL.

--- src/app/_interceptors/authorize.interceptor.ts
+++ src/app/_interceptors/authorize.interceptor.ts
@@ -1,6 +1,5 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
 import { empty, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from '../_services/auth.service';
@@ -9,7 +8,6 @@ import { AuthService } from '../_services/auth.service';
 export class AuthorizeInterceptor implements HttpInterceptor {
   constructor(
     private authService: AuthService,
-    private router: Router,
   ) { }
 
   public intercept(request: HttpRequest<any>, next: HttpHandler) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import environmentModel from './environment.model';
 
 export const environment: environmentModel = {
   production: true,
-  baseUrl: 'https://gauti-invoice-api.herokuapp.com/api'
+  baseUrl: '/api',
 };
